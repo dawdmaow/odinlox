3da07148a1f934873958dd9d6f4aa1628466ec2f
Dawid <dawidkotlin@gmail.com>
2026-03-30 16:06:56 +0200

    flatten web stuff

--- build_wasm.sh
+++ build_wasm.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 mkdir -p web
 odin_js="$(odin root)/core/sys/wasm/js/odin.js"
-cp "$odin_js" web/odin.js
+cp "$odin_js" odin.js
 # Verbose intern-table logging: ./build_wasm_debug.sh or add -define:LOX_TABLE_DEBUG=true
-odin build . -define:WASM=true -target:js_wasm32 -out:web/index.wasm -o:speed
+odin build . -define:WASM=true -target:js_wasm32 -out:index.wasm -o:speed
